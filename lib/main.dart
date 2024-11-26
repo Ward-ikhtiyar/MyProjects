4309a5813026856bd30396a1dc676c784512123c
Dada <dada.66.6x.outlook@gmail.com>
2024-11-26 23:31:19 +0300

    hello (i might commit someshit in android ) and the imports :D

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:SpeedOrder/MainPage.dart';
+import 'package:SpeedOrder/signInpage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_4/MainPage.dart';
-import 'package:flutter_application_4/signInpage.dart';
+
+
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
